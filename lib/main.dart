9e4734655f890c12913a75f89725eb827ab800f9
ZVinicius <zvinnicius@gmail.com>
2019-09-03 21:50:15 -0300

    exercicio finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,17 @@ class Home extends StatefulWidget{
    TextEditingController weigthController = TextEditingController();
    TextEditingController heightController = TextEditingController();
 
+   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
    String _infoText = "Informe Seus Dados.";
 
    void _resetFields(){
      weigthController.text = "";
      heightController.text = "";
-
-     _infoText = "Informe Seus Dados.";
+      setState(() {
+        _infoText = "Informe Seus Dados.";   
+        _formKey = GlobalKey<FormState>();
+      });     
    }
 
    void _calculate(){
@@ -39,10 +43,22 @@ class Home extends StatefulWidget{
         if(imc < 18.6){
           _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
         }
+        else if (imc >= 18.6 && imc <= 24.9){
+          _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
+        }
+        else if (imc >= 24.9 && imc <= 29.9){
+          _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+        }
+        else if (imc >= 29.9 && imc <= 34.9){
+          _infoText = "Obesidade grau I (${imc.toStringAsPrecision(4)})";
+        }
+        else if (imc >= 34.9 && imc <= 39.9){
+          _infoText = "Obesidade grau II (${imc.toStringAsPrecision(4)})";
+        }
+        else if (imc >=  40){
+          _infoText = "Obesidade grau III (${imc.toStringAsPrecision(4)})";
+        }        
      });
-     
-
-
    }
 
    @override
@@ -62,52 +78,69 @@ class Home extends StatefulWidget{
        backgroundColor: Colors.white,
        body: SingleChildScrollView(
          padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
-         child:
-          Column(
-          //isso vai fazer com que todos os itens da coluna preencham toda a tela no eixo "x"
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            //específicamente esse widget não vai ocupar toda linha dele pq foi estabelecido um tamanho para ele
-            Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Peso Kg",
-                labelStyle: TextStyle(color: Colors.green)
-                ),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-                controller: weigthController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.green)
-                ),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: heightController,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top:10.0, bottom: 10.0),
-                child: 
-                  Container(
-                    height: 50.0,
-                    child: RaisedButton(
-                      onPressed: _calculate,
-                      child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0)),
-                      color: Colors.green,
-                    ),
+         child: Form(
+           key: _formKey,
+           child: 
+              Column (
+                //isso vai fazer com que todos os itens da coluna preencham toda a tela no eixo "x"
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  //específicamente esse widget não vai ocupar toda linha dele pq foi estabelecido um tamanho para ele
+                  Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Peso Kg",
+                      labelStyle: TextStyle(color: Colors.green)
+                      ),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 25.0),
+                      controller: weigthController,
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Insira seu Peso!";
+                      }
+                    },
                   ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.green)
+                      ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: heightController,
+                    validator: (value){
+                        if(value.isEmpty){
+                          return "Insira sua altura";
+                        }
+                    }
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top:10.0, bottom: 10.0),
+                      child: 
+                        Container(
+                          height: 50.0,
+                          child: RaisedButton(
+                            onPressed: (){
+                              if(_formKey.currentState.validate()){
+                                _calculate();
+                              }  
+                            },
+                            child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                            color: Colors.green,
+                          ),
+                        ),
+                    ),
+                    Text(
+                      _infoText,
+                      textAlign: TextAlign.center,  
+                      style: TextStyle(fontSize: 25.0, color: Colors.green),
+                    )
+                ],
               ),
-              Text(
-                _infoText,
-                textAlign: TextAlign.center,  
-                style: TextStyle(fontSize: 25.0, color: Colors.green),
-              )
-          ],
-          ),
+         ),
        )
      );
    }
